f2fe506acef31ec6f2ab7bb2dcb95d19ccf68003
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-03-04 16:32:24 -0300

    Adicionando hello world

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+     return res.send('hello world')
+} )
+
 app.listen(3000, () => {
     console.log('servidor rodando na porta 3000');
 });
+
+
+
+
+
+//
