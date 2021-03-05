a3881af071f9d6a9e339a56f9864c9a9988119ab
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-03-05 00:50:08 -0300

    finalizando hello-wolrd

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-     return res.send('hello world')
-} )
+const listaUsuarios = [
+    'Ivens',
+    'Jailson',
+    'Maria',
+    'Natalia',
+    'Ariel',
+    'Nykolle'
+];
 
-app.listen(3000, () => {
-    console.log('servidor rodando na porta 3000');
+
+//query params
+//Ex: localhost:3000/usuarios?nome=Ivens
+app.get('/usuarios',(req, res) => {
+    
+    const { nome } = req.query;
+    let listaRetornp = listaUsuarios.filter(i => i.includes(nome || '' ));
+    return res.json(listaUsuarios);
+});
+
+//Route params
+//ex: ecomerce.com/produto/4/detalhes
+app.get('/:usuario', (req, res) => {
+    const { usuario } = req.params;
+     return res.send(`hello world ${usuario}`);
+} );
+
+app.listen(3100, () => {
+    console.log('servidor rodando na porta 3100');
 });
 
 
